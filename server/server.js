7db29c39d1c7b579c2b218908bfddd246b7e03b5
romanrogol <romanrogol21@gmail.com>
2025-06-15 19:01:03 +0300

    modified server.js

--- server/server.js
+++ server/server.js
@@ -114,7 +114,7 @@ app.post('/send-message', async (req, res) => {
 
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
