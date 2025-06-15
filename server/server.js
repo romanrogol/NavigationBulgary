f0090fd981257e196b8927418b5851d3125e6156
romanrogol <romanrogol21@gmail.com>
2025-06-15 22:46:55 +0300

    fix: trust proxy for express-rate-limit

--- server/server.js
+++ server/server.js
@@ -65,6 +65,7 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3001;
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
